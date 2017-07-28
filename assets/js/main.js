71fed37ee5368c6040680fc8f9978a52c2f4b30e
bryantran97@gmail.com <bryantran97@gmail.com>
2017-07-28 04:30:56 -0500

    finished for the most part, will come back to fix later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,25 @@
 var focusedAbout = false; /* To know when button is focused */
 var focusedProjects = false; /* To know when button is focused */
 
-
 /* This is for the About button */
 $('#about').click(function(){
-	$('#about-content').toggleClass('show').addClass('animated bounceInDown');
-	
 
 	// This if statement allows to see which part they are focused on
 	if(focusedAbout == true){
+		$('#about-content').removeClass('show');
 		$(this).text("About");
 		focusedAbout = false;
 	} else {
+		$('#about-content').addClass('show').addClass('animated bounceInDown');
 		$(this).text("[About]");
 		focusedAbout = true;
 	}
 });
+
+$('#resume').click(function(){
+	alert("I am not putting up my resume yet, still need to update. Come back next time!");
+});
+
 
 /* This is for the projects button */
 $('#projects').click(function(){
